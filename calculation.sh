2f8c697107b718f95400b1d465d37bfc10c40eab
riyazthk <er.riyaz2507@gmail.com>
2020-05-25 04:20:11 +0530

    To add a third calculation

--- calculation.sh
+++ calculation.sh
@@ -3,4 +3,6 @@ read -p "enter a first value :" a
 read -p "enter a second value :" b
 read -p "enter a third vslue :" c
 
-val=$(( ($a * $b)+$c ))
+val=$(( ($a+$b)*$c ))
+val1=$(( ($a*$b)+$c ))
+parttime=8
