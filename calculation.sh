dcb971b380c65d483f6f2b4d0506f81de2fa21aa
riyazthk <er.riyaz2507@gmail.com>
2020-05-25 05:30:03 +0530

    to define a array

--- calculation.sh
+++ calculation.sh
@@ -3,10 +3,16 @@ read -p "enter a first value :" a
 read -p "enter a second value :" b
 read -p "enter a third vslue :" c
 
-declare -A result
-
-result=$(( ($a+$b)*$c ))
-result=$(( ($a*$b)+$c ))
+declare -A val
+j=0
+result[ (j++) ]=$(( ($a+$b)*$c ))
+result[ (j++) ]=$(( ($a*$b)+$c ))
 parttime=8
-result=$(( $c +($a /$b) ))
-result=$(( ($a%$b)+$c ))
+result[ (j++) ]=$(( $c +($a /$b) ))
+result[ (j++) ]=$(( ($a%$b)+$c ))
+echo ${#result[@]}
+for i in "${!result[@]}"
+do
+echo "$i=>${result[$i]}"
+done
+
